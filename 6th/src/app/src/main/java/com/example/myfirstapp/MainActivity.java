1ffd48acfd00dd575c2433fa4fe048603a299441
kjmin622 <kjmin622@naver.com>
2020-05-05 20:22:11 +0900

    20200505-android src

--- 6th/src/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ 6th/src/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,6 +5,9 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -37,5 +40,32 @@ public class MainActivity extends AppCompatActivity {
             this.lastTimeBackPressed = System.currentTimeMillis();
             Toast.makeText(this,"뒤로가기 버튼을 한 번 더 누르시면 종료됩니다.",Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()) {
+            case R.id.menu0:
+                myWebView.loadUrl("https://kjmin622.github.io/");
+                break;
+            case R.id.menu1:
+                myWebView.loadUrl("https://huskycat1202.github.io/intro.html");
+                break;
+            case R.id.menu2:
+                myWebView.loadUrl("https://iopopoi.github.io/");
+                break;
+            case R.id.menu3:
+                myWebView.loadUrl("https://lee-sungmin.github.io/");
+                break;
+        }
+
+        return true;
     }
 }
