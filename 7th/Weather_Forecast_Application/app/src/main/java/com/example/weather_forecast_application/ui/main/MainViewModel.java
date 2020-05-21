038eca019aae52c60f447cd9959d2885b9042c82
kjmin622 <kjmin622@naver.com>
2020-05-21 15:16:57 +0900

    restart

---
+++ 7th/Weather_Forecast_Application/app/src/main/java/com/example/weather_forecast_application/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.example.weather_forecast_application.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
