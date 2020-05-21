3823fb7abeb56802314c9b799cf2671d54ab9c86
kjmin622 <kjmin622@naver.com>
2020-05-21 20:18:10 +0900

    7th 5/10

--- 7th/code/app/src/main/java/com/example/weather_forecast_application/MainActivity.java
+++ 7th/code/app/src/main/java/com/example/weather_forecast_application/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weather_forecast_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
